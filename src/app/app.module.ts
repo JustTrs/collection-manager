7e135dffba6bb559d282c267041ac3ed191f8566
JustTrs <justtrs@gmail.com>
2018-12-17 12:50:17 +0100

    Add Boostrap and FontAwesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+library.add(fab, fas);
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,6 +18,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FontAwesomeModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
